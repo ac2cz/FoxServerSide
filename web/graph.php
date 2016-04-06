cda50d46a17719f4036afa45290933d5b46e62db
Chris Thompson <chrisethompson@gmail.com>
2016-04-06 01:15:17 +0000

    Plot graph from Health page

--- web/graph.php
+++ web/graph.php
@@ -1,20 +1,96 @@
 <html>
 <head>
-<title>Fox Server Field Data</title>
-<link rel="stylesheet" type="text/css" media="all" href="http://www.amsat.org/wordpress/wp-content/themes/twentyeleven-amsat-child/style.css" />
-</head>
-<body>
-<img src='http://www.amsat.org/wordpress/wp-content/uploads/2014/08/amsat2.png'>
+  
+    <title>Fox Server Field Data</title>
+    <link rel="stylesheet" type="text/css" media="all" href="http://www.amsat.org/wordpress/wp-content/themes/twentyeleven-a
+msat-child/style.css" />
 
-<?php
+    <!-- Request data to server -->
+
+    <?php
    $arg1 = $_GET['sat'];
    $arg2 = $_GET['field'];
    $raw = $_GET['raw'];
    $reset = $_GET['reset'];
    $uptime = $_GET['uptime'];
    $num = $_GET['rows'];
+   $port = $_GET['port'];
    $raw='conv';
+   if ($port == "") $port = 8080;
    #echo ("http://localhost:8080/field/$arg1/$arg2/$raw/$num/$reset/$uptime");
-   $a = file_get_contents("http://localhost:8080/field/$arg1/$arg2/$raw/$num/$reset/$uptime");
-   echo ($a);
-?>
+   $a = file_get_contents("http://localhost:$port/field/$arg1/$arg2/$raw/$num/$reset/$uptime");
+
+        if ($a) {
+            // split html table to build array
+            $rows = preg_split('#<tr>#i', $a);
+            foreach ( $rows as $key => $row ) {
+                $rows[$key] = preg_split('#</td><td>#i', $row);
+                foreach ($rows[$key] as $key2 => $row2) {
+                // remove leftover html tags
+                    $rows[$key][$key2]=strip_tags($row2);
+                }
+            }
+        }
+        // delete first two rows (non-numeric)
+        array_shift($rows);
+        array_shift($rows);
+    ?>
+
+    <!-- Stuff needed to display plot through Google charts -->
+
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+        google.charts.load('current', {packages: ['corechart', 'line']});
+        google.charts.setOnLoadCallback(drawBasic);
+
+        function drawBasic() {
+            var data = new google.visualization.DataTable();
+            data.addColumn('number', 'Uptime');
+            data.addColumn('number', 'Value');
+          
+            // build array for plot. Get uptime and parameter value from php array
+            <?php foreach ( $rows as $key => $row ) { ?>
+                data.addRow([
+                    <?php echo $rows[$key][1];?>,
+                    <?php echo $rows[$key][2];?>
+                ]);
+            <?php }?>           
+ 
+            var options = {
+                hAxis: {
+                    title: 'Uptime'
+                },
+                vAxis: {
+                    title: '<?php echo $arg2;?>'
+                },
+                legend: {
+                    position: 'none'
+                },
+                titleTextStyle: {
+                    fontSize: 18
+                },
+                    title: 
+                        '<?php 
+                            echo 'Fox ', $arg1, 'A ', $arg2, ' ';
+                            if ($raw == 'conv')
+                                echo 'Converted';
+                            else echo 'Raw';
+                            echo ' Values, Reset = ', $rows[0][0];
+                        ?>'
+              
+            };
+
+            var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+            chart.draw(data, options);
+        }
+    </script>
+      
+    <body>
+        <img src='http://www.amsat.org/wordpress/wp-content/uploads/2014/08/amsat2.png'>
+        <div id="chart_div" style="width: 1000px; height: 500px"></div>
+    </body>
+  
+</head>
+</html>
+
+
