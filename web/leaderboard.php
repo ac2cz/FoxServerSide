97bd1918e1e51b9723f6d316f6cb682b6a537366
Chris Thompson <chrisethompson@gmail.com>
2016-04-19 01:11:35 +0000

    Updated leaderboard to have last weeks data

--- web/leaderboard.php
+++ web/leaderboard.php
@@ -17,6 +17,7 @@
     function latest($i) {
         global $DB, $conn, $PORT;
         $name=getName($i);
+        echo	"<a href=leaderboard.php?id=$i&db=$DB><strong>$name:</strong></a> <a href=health.php?id=$i&port=$PORT>latest spacecraft health </a> <br>";
         $sql = "select count(date_time) from STP_HEADER where id=$i and  timestampdiff(MINUTE,date_time,now()) < 90;";
         mysql_select_db($DB);
         $retval = mysql_query( $sql, $conn );
@@ -25,7 +26,7 @@
         }
  
         $row = mysql_fetch_array($retval, MYSQL_ASSOC);
-        echo "<strong>$name </strong>Frames Received last 90 mins : {$row['count(date_time)']} </br>";
+        echo "Frames last 90 mins : {$row['count(date_time)']} </br>";
         echo "From ground stations: <br>";
         $sql = "select distinct receiver from STP_HEADER where id=$i and timestampdiff(MINUTE,date_time,now()) < 90 order by resets desc, uptime desc;";
         mysql_select_db($DB);
@@ -66,7 +67,6 @@
 
           echo	"Total Frames Since Launch: {$row['count(receiver)']} <br>".
 	"<br>";
-          echo	"<strong>$name:</strong> <a href=health.php?id=$i&port=$PORT>latest spacecraft health </a> <br>";
           echo "<br>";
     }
 
@@ -94,7 +94,8 @@
    echo "<table cellspacing='0' cellpadding='0' width=1024 border='0'>";
    echo "<tr><td><strong>Ground station</strong></td>".
         "<td align='center'><strong>DUV Frames</strong></td>".
-        "<td align='center'><strong>HighSpeed Frames</strong></td>";
+        "<td align='center'><strong>9k6 Frames</strong></td>".
+        "<td align='center'><strong>Last 7 days</strong></td>";
    echo "<td rowspan=20>";
    if ($id=='A') {
       latest(1, $PORT);
@@ -105,7 +106,7 @@
    }
    echo "</td>";
    echo	"</tr>";
-   $sql = "select receiver, sum(case when source like '%duv' then 1 else 0 end) DUV, sum(case when source like '%highspeed' then 1 else 0 end) HighSpeed from STP_HEADER $where group by receiver order by DUV DESC";
+   $sql = "select receiver, sum(case when source like '%duv' then 1 else 0 end) DUV, sum(case when source like '%highspeed' then 1 else 0 end) HighSpeed, sum(case when timestampdiff(DAY,date_time,now()) < 7 then 1 else 0 end) last from STP_HEADER $where group by receiver order by DUV DESC";
    mysql_select_db($DB);
    $retval = mysql_query( $sql, $conn );
    if(! $retval ) {
@@ -116,7 +117,8 @@
    {
       echo "<tr><td><a href=ground_station.php?id=$id&db=$DB&station={$row['receiver']}>{$row['receiver']}</a></td>  ".
          "<td align='center'>{$row['DUV']}</td>".
-         "<td align='center'>{$row['HighSpeed']}</td> </tr> ";
+         "<td align='center'>{$row['HighSpeed']}</td> ".
+         "<td align='center'>{$row['last']}</td> </tr> ";
    }
    echo "</table>";
    mysql_close($conn);
