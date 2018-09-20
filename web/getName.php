3701b04017d7821a8656e54969b470d83876db05
Telem Manager <g0kla@arrl.net>
2018-09-20 00:14:42 +0000

    Added getName

---
+++ web/getName.php
@@ -0,0 +1,10 @@
+<?php
+    function getName($n) {
+        if ($n == 1) return "AO-85 (Fox-1A)";
+        if ($n == 2) return "AO-91 (RadFxSat)";
+        if ($n == 3) return "Fox-1Cliff";
+        if ($n == 4) return "Fox-1D";
+        if ($n == 5) return "Fox-1E (RadFxSat2)";
+        return "FOX";
+    }
+?>
