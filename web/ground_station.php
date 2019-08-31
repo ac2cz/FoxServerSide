3fcc1976a00a57d5c2eb054aa1d4f4c157501d66
Telem Manager <g0kla@arrl.net>
2019-08-31 13:25:54 +0000

    Initial test remediation of ground_station script

--- web/ground_station.php
+++ web/ground_station.php
@@ -8,12 +8,17 @@
 <?php
     $DB="FOXDB";
     $STATION="";
-    $dbhost = 'localhost:3036';
+    $dbhost = 'localhost';
     $dbuser = 'foxrpt';
     $dbpass = 'amsatfox';
     $id = $_GET['id'];
     $DB = $_GET['db'];
-    $STATION = $_GET['station'];
+    $station = $_GET['station'];
+    if (strpos($station, ' ') > 0) {
+        $STATION = substr($station, 0, strpos($station, ' '));  
+    } else {
+        $STATION = $station;
+    }
 
     if (!is_numeric($id)) { die("invalid paramater"); }
     if ($id == "") $id=1;
@@ -31,111 +36,53 @@
         $idwhere="and id=$id";
         echo "<h1 class=entry-title>FOX-$name: Ground Station $STATION - Last 7 days</h1>";
     }
-    $conn = mysql_connect($dbhost, $dbuser, $dbpass);
+    $mysqli = mysqli_connect($dbhost, $dbuser, $dbpass, $DB);
+    #$mysqli = mysqli_connect('localhost', $dbuser, $dbpass, $db);
+    if(mysqli_connect_errno($mysqli)) {
+       # Not to be inthe production code
+       #echo "Error: Failed to make a MySQL connection, here is why: <br>";
+       #echo "Errno: " . mysqli_connect_errno($mysqli) . "<br>";
+       #echo "Error: " . mysqli_connect_error($mysqli) . "<br>";
+        die("No Connection<br>");   
+    }
+    echo "<table cellspacing='0' cellpadding='0' width=1024 border='0'>";
+    echo "<tr><td><strong>Ground station</strong></td>".
+         "<td align='center'><strong>DUV Frames</strong></td>".
+         "<td align='center'><strong>HighSpeed Frames</strong></td>";
    
-   if(! $conn )
-   {
-      die('Could not connect: ' . mysql_error());
-   }
-   
-   echo "<table cellspacing='0' cellpadding='0' width=1024 border='0'>";
-   echo "<tr><td><strong>Ground station</strong></td>".
-        "<td align='center'><strong>DUV Frames</strong></td>".
-        "<td align='center'><strong>HighSpeed Frames</strong></td>";
-
-  $sql = sprintf("select count(date_time) from STP_HEADER where receiver='%s' %s and timestampdiff(MINUTE,date_time,now()) < 90;",$STATION, $idwhere);
-   mysql_select_db($DB);
-   $retval = mysql_query( $sql, $conn );
-   if(! $retval ) {
-      die('Could not get 90 min data: ' . mysql_error());
-   }
- 
-   $row = mysql_fetch_array($retval, MYSQL_ASSOC);
-   echo "<td rowspan=20><strong>Frames Received last 90 mins : </strong>{$row['count(date_time)']} </br>";
-
-  $sql = sprintf("select count(date_time) from STP_HEADER where receiver='%s' %s and timestampdiff(HOUR,date_time,now()) < 24",$STATION, $idwhere);;
-   mysql_select_db($DB);
-   $retval = mysql_query( $sql, $conn );
-   if(! $retval ) {
-      die('Could not get data: ' . mysql_error());
-   }
- 
-   $row = mysql_fetch_array($retval, MYSQL_ASSOC);
-   echo "<strong>Frames Received last 24 hours:</strong> {$row['count(date_time)']} </br>";
-
-   echo	"</td>".
-	"</tr>";
-   $sql = sprintf("select receiver, sum(case when source like '%%duv' then 1 else 0 end) DUV, sum(case when source like '%%highspeed' then 1 else 0 end) HighSpeed from STP_HEADER where receiver='%s' %s and timestampdiff(DAY,date_time,now()) < 7 ",$STATION, $idwhere);
-   mysql_select_db($DB);
-   $retval = mysql_query( $sql, $conn );
-   if(! $retval ) { die('Could not get data: ' . mysql_error()); }
-   
-   while($row = mysql_fetch_array($retval, MYSQL_ASSOC))
-   {
-      echo "<tr><td>{$row['receiver']}</td>  ".
+    $sql = sprintf("select count(date_time) from STP_HEADER where receiver='%s' %s and timestampdiff(MINUTE,date_time,now()) < 90;",$STATION, $idwhere);
+    if ($result = mysqli_query($mysqli, $sql )) {
+        $row = mysqli_fetch_assoc($result);
+        echo "<td rowspan=20><strong>Frames Received last 90 mins : </strong>{$row['count(date_time)']} </br>";
+        mysqli_free_result($retval);
+    } else {
+        die('Could not get data: ');
+        #die('Could not get data: ' . mysqli_error($mysqli));
+    }
+    $sql = sprintf("select count(date_time) from STP_HEADER where receiver='%s' %s and timestampdiff(HOUR,date_time,now()) < 24",$STATION, $idwhere);;
+    if ($result = mysqli_query($mysqli, $sql )) {
+        $row = mysqli_fetch_assoc($result);
+         echo "<strong>Frames Received last 24 hours:</strong> {$row['count(date_time)']} </br>";
+         echo "</td></tr>";
+        mysqli_free_result($retval);
+    } else {
+        die('Could not get data: ');
+    }
+    
+    $sql = sprintf("select receiver, sum(case when source like '%%duv' then 1 else 0 end) DUV, sum(case when source like '%%highspeed' then 1 else 0 end) HighSpeed from STP_HEADER where receiver='%s' %s and timestampdiff(DAY,date_time,now()) < 7 ",$STATION, $idwhere);
+    if ($result = mysqli_query($mysqli, $sql )) {
+        $row = mysqli_fetch_assoc($result);
+        echo "<tr><td>{$row['receiver']}</td>  ".
          "<td align='center'>{$row['DUV']}</td>".
          "<td align='center'>{$row['HighSpeed']}</td> </tr> ";
-   }
-
-   echo "</table>";
-   
-if ($id != 0) {
-   $sql = sprintf("select count(*) as count from Fox%dRTTELEMETRY t, STP_HEADER s where t.id=%d and t.id=s.id and s.type=1 and t.resets=s.resets and t.uptime=s.uptime and s.receiver='%s' and timestampdiff(DAY,date_time,now()) < 7 ",$id,$id,$STATION);
-   mysql_select_db($DB);
-   $retval = mysql_query( $sql, $conn );
-   if(! $retval ) { die('Could not get data: ' . mysql_error()); }
-   $row = mysql_fetch_array($retval, MYSQL_ASSOC);
-   $RTPAYLOADS="{$row['count']}";
-   echo "<br>Real Time Payloads: $RTPAYLOADS </br>";
-
-   $sql = sprintf("select count(*) as count from Fox%dMAXTELEMETRY t, STP_HEADER s where t.id=%d and t.id=s.id and s.type=2 and t.resets=s.resets and t.uptime=s.uptime and s.receiver='%s' and timestampdiff(DAY,date_time,now()) < 7 ",$id,$id,$STATION);
-   mysql_select_db($DB);
-   $retval = mysql_query( $sql, $conn );
-   if(! $retval ) { die('Could not get data: ' . mysql_error()); }
-   $row = mysql_fetch_array($retval, MYSQL_ASSOC);
-   $MAXPAYLOADS="{$row['count']}";
-   echo "Max Payloads: $MAXPAYLOADS </br>";
-
-   $sql = sprintf("select count(*) as count from Fox%dMINTELEMETRY t, STP_HEADER s where t.id=%d and t.id=s.id and s.type=3 and t.resets=s.resets and t.uptime=s.uptime and s.receiver='%s' and timestampdiff(DAY,date_time,now()) < 7 ",$id,$id,$STATION);
-   mysql_select_db($DB);
-   $retval = mysql_query( $sql, $conn );
-   if(! $retval ) { die('Could not get data: ' . mysql_error()); }
-   $row = mysql_fetch_array($retval, MYSQL_ASSOC);
-   $MINPAYLOADS="{$row['count']}";
-   echo "Min Payloads: $MINPAYLOADS </br>";
-
-   $sql = sprintf("select count(*) as count from Fox%dRADTELEMETRY t, STP_HEADER s where t.id=%d and t.id=s.id and s.type=4 and t.resets=s.resets and t.uptime=s.uptime and s.receiver='%s' and timestampdiff(DAY,date_time,now()) < 7 ",$id,$id,$STATION);
-   mysql_select_db($DB);
-   $retval = mysql_query( $sql, $conn );
-   if(! $retval ) { die('Could not get data: ' . mysql_error()); }
-   $row = mysql_fetch_array($retval, MYSQL_ASSOC);
-   $RADPAYLOADS="{$row['count']}";
-   $TOTAL=$RTPAYLOADS + $MAXPAYLOADS + $MINPAYLOADS ;
-   echo "<strong>Total Fox Telemetry Payloads:</strong> $TOTAL </br>";
-   echo "Experiment Payloads: $RADPAYLOADS </br>";
-   $TOTAL=$RTPAYLOADS + $MAXPAYLOADS + $MINPAYLOADS + $RADPAYLOADS;
-   echo "<strong>Total Payloads:</strong> $TOTAL </br>";
-}
-   echo "<p><h2>Using Demodulator:</h2>";
-   $sql = sprintf("select demodulator from STP_HEADER where receiver='%s' %s order by date_time DESC limit 1",$STATION, $idwhere);
-   mysql_select_db($DB);
-   $retval = mysql_query( $sql, $conn );
-   if(! $retval ) { die('Could not get data: ' . mysql_error()); }
-   $row = mysql_fetch_array($retval, MYSQL_ASSOC);
-   echo "- {$row['demodulator']}<br> ";
-
-   echo "<p><h2>Station Receiver(s):</h2>";
-   $sql = sprintf("select distinct(receiver_rf) from STP_HEADER where receiver_rf != 'NONE' and receiver='%s' %s",$STATION, $idwhere);
-   mysql_select_db($DB);
-   $retval = mysql_query( $sql, $conn );
-   if(! $retval ) { die('Could not get data: ' . mysql_error()); }
-   
-   while($row = mysql_fetch_array($retval, MYSQL_ASSOC))
-   {
-      echo "- {$row['receiver_rf']}<br> ";
-   }
-
-   mysql_close($conn);
+        mysqli_free_result($retval);
+    } else {
+        die('Could not get data: ');
+    }
+ 
+    echo "</table>";
+    mysqli_close($mysqli);
 ?>
+<h3>Remaining functionality it temporarily unavailable</h3>
 </body>
 </html>
