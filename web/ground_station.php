744446a68022c144ea0eb9f617653aada2da6e36
Telem Manager <g0kla@arrl.net>
2019-08-31 14:42:17 +0000

    Ground station.php ported to mysqli

--- web/ground_station.php
+++ web/ground_station.php
@@ -54,7 +54,7 @@
     if ($result = mysqli_query($mysqli, $sql )) {
         $row = mysqli_fetch_assoc($result);
         echo "<td rowspan=20><strong>Frames Received last 90 mins : </strong>{$row['count(date_time)']} </br>";
-        mysqli_free_result($retval);
+        mysqli_free_result($result);
     } else {
         die('Could not get data: ');
         #die('Could not get data: ' . mysqli_error($mysqli));
@@ -64,25 +64,85 @@
         $row = mysqli_fetch_assoc($result);
          echo "<strong>Frames Received last 24 hours:</strong> {$row['count(date_time)']} </br>";
          echo "</td></tr>";
-        mysqli_free_result($retval);
+        mysqli_free_result($result);
     } else {
         die('Could not get data: ');
     }
     
     $sql = sprintf("select receiver, sum(case when source like '%%duv' then 1 else 0 end) DUV, sum(case when source like '%%highspeed' then 1 else 0 end) HighSpeed from STP_HEADER where receiver='%s' %s and timestampdiff(DAY,date_time,now()) < 7 ",$STATION, $idwhere);
     if ($result = mysqli_query($mysqli, $sql )) {
-        $row = mysqli_fetch_assoc($result);
-        echo "<tr><td>{$row['receiver']}</td>  ".
-         "<td align='center'>{$row['DUV']}</td>".
-         "<td align='center'>{$row['HighSpeed']}</td> </tr> ";
-        mysqli_free_result($retval);
+        while($row = mysqli_fetch_assoc($result)) {
+            echo "<tr><td>{$row['receiver']}</td>  ".
+             "<td align='center'>{$row['DUV']}</td>".
+             "<td align='center'>{$row['HighSpeed']}</td> </tr> ";
+        }
+        mysqli_free_result($result);
     } else {
         die('Could not get data: ');
     }
  
     echo "</table>";
+
+    if ($id != 0) {
+       echo "<p><h2>Payloads from this Spacecraft:</h2>";
+
+       $sql = sprintf("select count(*) as count from Fox%dRTTELEMETRY t, STP_HEADER s where t.id=%d and t.id=s.id and s.type=1 and t.resets=s.resets and t.uptime=s.uptime and s.receiver='%s' and timestampdiff(DAY,date_time,now()) < 7 ",$id,$id,$STATION);
+        if ($result = mysqli_query($mysqli, $sql )) {
+            $row = mysqli_fetch_assoc($result);
+            $RTPAYLOADS="{$row['count']}";
+            echo "Real Time Payloads: $RTPAYLOADS <br>";
+            mysqli_free_result($result);
+        } else { die('Could not get data: '); }
+    
+       $sql = sprintf("select count(*) as count from Fox%dMAXTELEMETRY t, STP_HEADER s where t.id=%d and t.id=s.id and s.type=2 and t.resets=s.resets and t.uptime=s.uptime and s.receiver='%s' and timestampdiff(DAY,date_time,now()) < 7 ",$id,$id,$STATION);
+        if ($result = mysqli_query($mysqli, $sql )) {
+            $row = mysqli_fetch_assoc($result);
+            $MAXPAYLOADS="{$row['count']}";
+            echo "Max Payloads: $MAXPAYLOADS </br>";
+            mysqli_free_result($result);
+        } else { die('Could not get data: '); }
+    
+       $sql = sprintf("select count(*) as count from Fox%dMINTELEMETRY t, STP_HEADER s where t.id=%d and t.id=s.id and s.type=3 and t.resets=s.resets and t.uptime=s.uptime and s.receiver='%s' and timestampdiff(DAY,date_time,now()) < 7 ",$id,$id,$STATION);
+        if ($result = mysqli_query($mysqli, $sql )) {
+            $row = mysqli_fetch_assoc($result);
+            $MINPAYLOADS="{$row['count']}";
+            echo "Min Payloads: $MINPAYLOADS </br>";
+            mysqli_free_result($result);
+        } else { die('Could not get data: '); }
+    
+       $sql = sprintf("select count(*) as count from Fox%dRADTELEMETRY t, STP_HEADER s where t.id=%d and t.id=s.id and s.type=4 and t.resets=s.resets and t.uptime=s.uptime and s.receiver='%s' and timestampdiff(DAY,date_time,now()) < 7 ",$id,$id,$STATION);
+        if ($result = mysqli_query($mysqli, $sql )) {
+            $row = mysqli_fetch_assoc($result);
+            $RADPAYLOADS="{$row['count']}";
+            $TOTAL=$RTPAYLOADS + $MAXPAYLOADS + $MINPAYLOADS ;
+            echo "<strong>Total Fox Telemetry Payloads:</strong> $TOTAL </br>";
+            echo "Experiment Payloads: $RADPAYLOADS </br>";
+            $TOTAL=$RTPAYLOADS + $MAXPAYLOADS + $MINPAYLOADS + $RADPAYLOADS;
+            echo "<strong>Total Payloads:</strong> $TOTAL </br>";
+            mysqli_free_result($result);
+        } else { die('Could not get data: '); }
+    }
+
+    # Now display the station details that we have stored
+    echo "<p><h2>Using Demodulator:</h2>";
+    $sql = sprintf("select demodulator from STP_HEADER where receiver='%s' %s order by date_time DESC limit 1",$STATION, $idwhere);
+    if ($result = mysqli_query($mysqli, $sql )) {
+        $row = mysqli_fetch_assoc($result);
+        echo "- {$row['demodulator']}<br> ";
+        mysqli_free_result($result);
+    } else { die('Could not get data: '); }
+ 
+    echo "<p><h2>Station Receiver(s):</h2>";
+    $sql = sprintf("select distinct(receiver_rf) from STP_HEADER where receiver_rf != 'NONE' and receiver='%s' %s",$STATION, $idwhere);
+    if ($result = mysqli_query($mysqli, $sql )) {
+        while($row = mysqli_fetch_assoc($result)) {
+            echo "- {$row['receiver_rf']}<br> ";
+        }
+        mysqli_free_result($result);
+    } else { die('Could not get data: '); }
+ 
+
     mysqli_close($mysqli);
 ?>
-<h3>Remaining functionality it temporarily unavailable</h3>
 </body>
 </html>
