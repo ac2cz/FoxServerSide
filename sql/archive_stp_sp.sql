e536b74d120577c0bd54eead03d7f2343d11c9d4
Telem Manager <g0kla@arrl.net>
2021-03-06 16:00:20 +0000

    Ignore duplicates in archive and simplify 30 day logic

--- sql/archive_stp_sp.sql
+++ sql/archive_stp_sp.sql
@@ -1,27 +1,23 @@
-DROP PROCEDURE IF EXISTS ArchiveSTPHeaders;
+DROP PROCEDURE IF EXISTS ArchiveSTPHeaders_v2;
 
 DELIMITER //
-CREATE PROCEDURE ArchiveSTPHeaders()
+CREATE PROCEDURE ArchiveSTPHeaders_v2()
 BEGIN
 
-DECLARE stp_header_archive_max_date timestamp ;
 DECLARE stp_header_max_date timestamp;
 
 start transaction;
 
-select max(date_time) from STP_HEADER into stp_header_max_date;
-select max(date_time) from STP_HEADER_ARCHIVE into stp_header_archive_max_date;
-if (ISNULL(stp_header_archive_max_date)) then set stp_header_archive_max_date = '2010-01-01 00:00:00';
-end if;
-select stp_header_archive_max_date;
+select now() into stp_header_max_date;
 
-insert into STP_HEADER_ARCHIVE
+/* Insert old records into archive */
+insert ignore into STP_HEADER_ARCHIVE
 (stpDate, id, resets, uptime, type, sequenceNumber, length, source, receiver, frequency, rx_location, receiver_rf, demodulator, measuredTCA, measuredTCAfrequency, date_time)
 (select stpDate, id, resets, uptime, type, sequenceNumber, length, source, receiver, frequency, rx_location, receiver_rf, demodulator, measuredTCA, measuredTCAfrequency, date_time
 from STP_HEADER
-where STP_HEADER.date_time > stp_header_archive_max_date
-and timestampdiff(DAY, date_time, stp_header_max_date) > 30);
+where timestampdiff(DAY, date_time, stp_header_max_date) > 30);
 
+/* Reset the archive count which we use to make leaderboard go fast*/
 delete from STP_HEADER_ARCHIVE_COUNT;
 
 insert into STP_HEADER_ARCHIVE_COUNT
@@ -32,14 +28,15 @@ sum(case when source like '%highspeed' then 1 else 0 end) highspeed_total,
 sum(case when source like '%bpsk' then 1 else 0 end) psk_total 
 from STP_HEADER_ARCHIVE group by id, receiver);
 
+/* Reset the Archive totals which are by spacecraft */
 delete from STP_ARCHIVE_TOTALS;
 insert into STP_ARCHIVE_TOTALS
 (id, total)
 (select id, count(id) from STP_HEADER_ARCHIVE group by id);
 
+/* Purge the records we moved */
 delete from STP_HEADER
-where STP_HEADER.date_time > stp_header_archive_max_date
-and timestampdiff(DAY, date_time, stp_header_max_date) > 30;
+where timestampdiff(DAY, date_time, stp_header_max_date) > 30;
 
 commit;
 
