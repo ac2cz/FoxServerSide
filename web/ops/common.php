7ff9752d72537f7d5499d8d829dc270855d95065
Telem Manager <g0kla@arrl.net>
2021-06-27 15:18:02 +0000

    Update

--- web/ops/common.php
+++ web/ops/common.php
@@ -2,7 +2,7 @@
 
     // These variables define the connection information for your MySQL database 
     $username = "g0kla"; 
-    $password = "adf8G5jkadFa84kjaq"; 
+    $password = "1234567890"; 
     $host = "localhost"; 
     $dbname = "FOXDB"; 
 
